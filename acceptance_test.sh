93b2e574334ee84a7230b12d75f9cc4941293f4f
Tomasz Jarmuż <tomasz.jarmuz@gmail.com>
2018-12-02 16:20:22 +0100

    corrected test return value

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
 echo $CALCULATOR_PORT
-test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 4
+test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
