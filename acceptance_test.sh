f417e9fc77affc4eeaa695812640cd5bcec9c029
Tomasz Jarmuż <tomasz.jarmuz@gmail.com>
2018-12-09 22:34:20 +0100

    fix ansiblePlaybook in Jenkinsfile 9

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "Running acceptance test..."
+echo "Running acceptance test... $0, $1, $2"
 whoami
-CALCULATOR_PORT=$(sshpass -p '$0' ssh -o StrictHostKeychecking=no eltomas@$1 "docker-compose port calculator 8080 | cut -d: -f2")
-echo "Host: $1:$CALCULATOR_PORT"
+CALCULATOR_PORT=$(sshpass -p $1 ssh -o StrictHostKeychecking=no eltomas@$2 "docker-compose port calculator 8080 | cut -d: -f2")
+echo "Host: $2:$CALCULATOR_PORT"
 #test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
-./gradlew acceptanceTest -Dcalculator.url=http://$1:$CALCULATOR_PORT
+./gradlew acceptanceTest -Dcalculator.url=http://$2:$CALCULATOR_PORT
